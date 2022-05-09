ccfa01eb90e6c0e47458c160278a5b34d2e24ae4
“Odaliz <odaliz.rojasroldan@aiesec.net>
2022-05-09 17:47:34 -0400

    Agregando configuracion Json al constructor de la clase Startup

--- ProveedoresGrupoD/ProveedoresGrupoD/Startup.cs
+++ ProveedoresGrupoD/ProveedoresGrupoD/Startup.cs
@@ -9,17 +9,24 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
 using System.Linq;
 using System.Threading.Tasks;
 using Logic.Managers;
+using Microsoft.OpenApi.Models;
 
 namespace ProveedoresGrupoD
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IWebHostEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
